753b3d0cdcc38443496a16729fbdc244d56fea3d
najia <najia.khurshid@gmail.com>
2015-05-25 12:47:58 +0500

    Class lecture and assignment

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-var a = 1;
+var obj = { name: "jia", age: 25 };
+obj = { name: "teh", name1: "Tehmina", age: 25 };
+obj["class"] = 23;
+var obj2 = { name: "lil", age: 23 };
+obj = {}; /// this <any> allows you to overwrite the mandatory requirements of this object if such a condition occurs 
+// where as if we create a blank object it treats it as any you can enter whatever you want in it 
+var a = {};
+// where as if we specify the data type while creating a null object it wont let us enter anything 
+var b1 = { name: "bill" };
+// number type array
+var numarray = [1, 2, 3];
+// if we create an array of type any, we can add values belonging to any data type 
+var anyarray = [1, "class"];
